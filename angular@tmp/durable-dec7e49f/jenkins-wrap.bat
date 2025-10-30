b1777e69c350ae2ba11a1f3d5882d7cf238e1ab5
Vedant Chaudhari <vedant.chaudhari@irisbusiness.com>
2025-10-30 11:58:58 +0530

    feat: working now

--- angular@tmp/durable-dec7e49f/jenkins-wrap.bat
+++
@@ -1,3 +0,0 @@
-@echo off 
-cmd /c call "D:/iris-code/ci-cd-demo\angular@tmp\durable-dec7e49f\jenkins-main.bat" > "D:/iris-code/ci-cd-demo\angular@tmp\durable-dec7e49f\jenkins-log.txt" 2>&1
-echo %ERRORLEVEL% > "D:/iris-code/ci-cd-demo\angular@tmp\durable-dec7e49f\jenkins-result.txt"
